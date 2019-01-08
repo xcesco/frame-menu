755ee0d0e69e69e4bff81ed62350594c965dd94e
xcesco <abubusoft@gmail.com>
2019-01-08 17:25:55 +0100

    @wip

--- web/config.js
+++ web/config.js
@@ -1,6 +1,12 @@
 /**
  * Identificativo dello spreadsheet
  *
+ * https://docs.google.com/spreadsheets/d/1g_iyG-1Xc1eY5-zFwjLkE64QbRrsie9wlA6jY1uZJeI/edit#gid=0
+ *
+ * Per invocare il menu con un altro foglio invocarlo con il parametro in get NomeFoglio. Esempio
+ *
+ *  http://localhost:63342/frame-menu/menu.html?NomeFoglio=foglio2
+ *
  * @type {string}
  */
 var SPREADSHEET_ID = '1g_iyG-1Xc1eY5-zFwjLkE64QbRrsie9wlA6jY1uZJeI';
