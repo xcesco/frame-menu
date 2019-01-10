c9712bfb89f4150a93caf2d6ceaa6694b5d5462c
xcesco <abubusoft@gmail.com>
2019-01-10 22:25:35 +0100

    @wip

--- web/app.js
+++ web/app.js
@@ -1,24 +1,3 @@
-/**
- * Identificativo dello spreadsheet
- *
- * https://docs.google.com/spreadsheets/d/1g_iyG-1Xc1eY5-zFwjLkE64QbRrsie9wlA6jY1uZJeI/edit#gid=0
- *
- * Per invocare il menu con un altro foglio invocarlo con il parametro in get NomeFoglio. Esempio
- *
- *  http://localhost:63342/frame-menu/menu.html?NomeFoglio=foglio2
- *
- * @type {string}
- */
-var SPREADSHEET_ID = '1g_iyG-1Xc1eY5-zFwjLkE64QbRrsie9wlA6jY1uZJeI';
-
-
-/**
- * Questa costante rappresenta il nome del foglio da utilizzare. NON SERVE MODIFICARE
- * @type {string}
- */
-const NOME_FOGLIO_PARAM = 'NomeFoglio';
-
-
 let menuInfo = null;
 let menuItems = [];
 let menuSheetList = [];
